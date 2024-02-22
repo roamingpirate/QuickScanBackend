967ab12ecee79bddacb4c43932d296fa6fb1ab7d
ankit <agentinfinity789@gmail.com>
2024-02-22 21:46:27 +0530

    jshs

--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -3,11 +3,27 @@ const router = express.Router();
 const auth = require("../Utils/auth");
 const classController = require("../controller/classController");
 
-router.post("/create", auth.authenticateToken, classController.createClass)
-router.get("/:classID", auth.authenticateToken, classController.getClassData)
-router.get("/activateQR/:classID", auth.authenticateToken, classController.activateQR)
-router.get("/deactivateQR/:classID", auth.authenticateToken, classController.deactivateQR)
-router.get("/addStudent/:classID", auth.authenticateToken, classController.addStudent)
-router.get("/getStudentsList/:classID", auth.authenticateToken, classController.getStudentList)
+router.post("/create", auth.authenticateToken, classController.createClass);
+router.get("/:classID", auth.authenticateToken, classController.getClassData);
+router.get(
+  "/activateQR/:classID",
+  auth.authenticateToken,
+  classController.activateQR
+);
+router.get(
+  "/deactivateQR/:classID",
+  auth.authenticateToken,
+  classController.deactivateQR
+);
+router.get(
+  "/addStudent/:classID",
+  auth.authenticateToken,
+  classController.addStudent
+);
+router.get(
+  "/getStudentsList/:classID",
+  auth.authenticateToken,
+  classController.getStudentList
+);
 
 module.exports = router;
